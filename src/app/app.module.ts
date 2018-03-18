c158ade7dc36b3da823400bfee235a71efc71927
Jason Bedard <jason.bedard@allocadia.com>
2018-03-17 22:26:05 -0700

    add ngrx/store-edevtools in non-prod environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { HomeComponent } from '../sections/home/home.component';
 import { ComponentsModule } from '../components/components.module';
@@ -9,11 +11,11 @@ import { SectionsModule } from '../sections/sections.module';
 import { PageNotFoundComponent } from '../sections/notfound/notfound.component';
 import { reducers, effects } from '../store';
 import { groupReducer } from '../store/reducers/group.reducer';
-import { EffectsModule } from '@ngrx/effects';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from '../sections/app/app.component';
 import { PlayersComponent } from '../sections/group/players/players.component';
 import { MatchesComponent } from '../sections/group/matches/matches.component';
+import { environment } from '../environments/environment';
 
 const appRoutes: Routes = [
   {
@@ -45,6 +47,7 @@ const appRoutes: Routes = [
     ),
     StoreModule.forRoot(reducers),
     EffectsModule.forRoot(effects),
+    environment.production ? [] : StoreDevtoolsModule.instrument({maxAge: 50}),
     HttpClientModule,
     BrowserModule,
     SectionsModule
